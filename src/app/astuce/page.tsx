6cabfedbf47651a5e1cf8138492d6232a7e24860
Michel Christophe <mchristopheweb@gmail.com>
2024-05-25 18:38:40 +0200

    ajout de la page astuce

---
+++ src/app/astuce/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function Astuce() {
+  return (
+    <main className="flex min-h-screen flex-col py-10 items-center">
+      <h1 className="text-2xl font-bold mb-6">P<span className="text-violet">A</span>S DE CODE PROMO !?!</h1>
+      <section className="flex flex-col items-center">
+        <h2 className="text-xl">Vous allez <span className="text-violet">t</span>out de même pas payer 😜</h2>
+        <p>Le code p<span className="text-violet">r</span>omotionel est caché dans cette page.</p>
+        <p>Pour <span className="text-violet">g</span>agner le droit de partir sans payer il faut le tro<span className="text-violet">u</span>ver.</p>
+        <p>Promis ce n'es<span className="text-violet">t</span> pas très compl<span className="text-violet">i</span>qué.</p>
+        <Link
+            href="/panier"
+            title="retour au panier" 
+            className="flex flex-col items-center text-violet mt-6"
+        >
+            <FaShoppingCart size={ 40 }/>
+            retour au panier
+        </Link>
+    </section>
+    </main>
+  );
+}
