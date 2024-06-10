5b5b50c7bbf1671f03e4b58bf287fc8ee490e56d
Michel Christophe <mchristopheweb@gmail.com>
2024-06-10 13:12:56 +0200

    mise en place du composant RedirectIfNotAllow pour la page astuce

--- src/app/panier/components/cartPresentation/Summary/validateCommande/ValidateCommande.tsx
+++ src/app/panier/components/cartPresentation/Summary/validateCommande/ValidateCommande.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { useEmptyTheCart, useNavigateTo, usePromoCode, useValidateTheOrder } from '@/app/_hooks/hooks';
+import { useNavigateTo, usePromoCode, useValidateTheOrder } from '@/app/_hooks/hooks';
 
 type Props = {}
 // essayer de voir pourquoi le cart se vide quand on va direct sur /commande_valide
@@ -9,13 +9,11 @@ function ValidateCommande({}: Props) {
 
   const validateTheOrder = useValidateTheOrder();
   const promoCodeIsValide = usePromoCode();
-  const emptyTheCart = useEmptyTheCart();
   const navigateTo = useNavigateTo();
   const path = promoCodeIsValide ? "/commande_valide" : "/astuce";
 
   const validateOrder = () => { 
     validateTheOrder();
-    promoCodeIsValide && emptyTheCart();
     navigateTo(path);
   }
 
